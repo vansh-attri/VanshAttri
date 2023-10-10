a30492b05440d47692b3c71a3e3708b90ffe312b
Vansh Attri <vanshattri123@gmail.com>
2023-10-10 21:59:27 +0530

    added responsiveness

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('toggle_button')[0]
+const navbarlinks = document.getElementsByClassName('links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarlinks.classList.toggle('active')
+})
